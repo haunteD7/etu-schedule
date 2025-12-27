a9ff48182e8284c02059f716fd1b153b0199e3c7
haunteD7 <228pacan228aue@gmail.com>
2025-12-28 00:03:16 +0300

    add week schedule

--- ScheduleClient.py
+++ ScheduleClient.py
@@ -1,16 +1,86 @@
-import json
 import requests
+import logging
+
+ERROR_MESSAGE = 'Произошла какая-то ошибка'
+NOTHING_FOUND_MESSAGE = 'Для данной группы нет, либо не найдено расписания'
+
+DELIM = '------------------------\n' 
+
+def format_lesson_form(form: str):
+    match form:
+       case "standard":
+           return "Обычно"
+       case "distant":
+           return "Дистанционно"
+       case "online":
+           return "Онлайн"
+    return ''
+
+logger = logging.getLogger(__name__)
 
 class ScheduleClient:
     def __init__(self, base_url: str):
         self.__base_url: str = base_url
 
-    def request_schedule_week(self, group: int) -> str:
-        response = requests.get(self.__base_url + '/mobile/schedule', params={ 
-            'groupNumber': group, 'joinWeeks': 'false' })
+    @staticmethod
+    def __format_lesson_time(lesson) -> str:
+       return f'{lesson['start_time']}-{lesson['end_time']}'
+    @staticmethod
+    def __format_lesson(lesson) -> str:
+        teacher = ''
+        if lesson['teacher']:
+            teacher = 'Преподаватель: ' + lesson['teacher'] + '\n'
+        room = ''
+        if lesson['room']:
+            room = 'Аудитория: ' + lesson['room'] + '\n'
+        form = ''
+        if lesson['form']:
+            form = 'Тип: ' + format_lesson_form(lesson['form']) + '\n'
+        return f'{DELIM}{ScheduleClient.__format_lesson_time(lesson)}:\n{lesson['name']} ({lesson['subjectType']}):\n{teacher}{room}{form}{DELIM}'
+    def request_schedule_week(self, group: int, week_number: int) -> str:
+        try:
+            response = requests.get(self.__base_url + '/mobile/schedule', params = { 
+                'groupNumber': group, 'joinWeeks': 'false' })
+            response.raise_for_status()
+        except requests.exceptions.HTTPError as http_err:
+            logger.error(f"HTTP error: {http_err}\nCode: {response.status_code}")
+            return ERROR_MESSAGE 
+        except requests.exceptions.ConnectionError as conn_err:
+            logger.error(f"Connection error: {conn_err}")
+            return ERROR_MESSAGE 
+        except requests.exceptions.Timeout as timeout_err:
+            logger.error(f"Request timeout: {timeout_err}")
+            return ERROR_MESSAGE 
+        except requests.exceptions.RequestException as req_err:
+            logger.error(f"Common request error: {req_err}")
+            return ERROR_MESSAGE 
+        except Exception as e:
+            print(f"Unexpected error: {e}")
+            return ERROR_MESSAGE 
 
-        # days = response.json()[str(group)]['days']
+        try:
+            response_json = response.json()
+        except ValueError as json_err:
+            logger.error(f"JSON parsing error: {json_err}")
+            return ERROR_MESSAGE 
 
-        # for i in range(6):
-        #     day = days[str(i)]
-        #     print(f'{day['name']}')
+        if response_json == {}:
+            return NOTHING_FOUND_MESSAGE
+        response_str = ''
+
+        days = response_json[str(group)]['days']
+        for i in range(6):
+            day = days[str(i)]
+            lessons = day['lessons']
+            lessons_str = ''
+
+            for j in range(len(lessons)):
+                lesson = lessons[j]
+                week = int(lesson['week'])
+                if week_number == week:
+                    lessons_str += self.__format_lesson(lesson)
+
+                
+            response_str += f'{day['name']}\n{lessons_str}\n' 
+        
+        return response_str 
--- main.py
+++ main.py
@@ -16,10 +16,9 @@ from telegram.ext import (
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-ENTER_GROUP, SELECT_SCHEDULE_TYPE, SELECT_DAY = range(3)
+ENTER_GROUP, SELECT_SCHEDULE_TYPE, SELECT_DAY, SELECT_WEEK = range(4)
 
 sc = ScheduleClient('https://digital.etu.ru/api')
-sc.request_schedule_week(4351)
 
 select_schedule_type_keyboard = InlineKeyboardMarkup([
     [
@@ -27,19 +26,34 @@ select_schedule_type_keyboard = InlineKeyboardMarkup([
         InlineKeyboardButton(text='На конкретный день', callback_data='schedule_day'),
     ],
 ])
+select_week_keyboard = InlineKeyboardMarkup([
+    [
+        InlineKeyboardButton(text='Нечетная неделя', callback_data='week_odd'),
+        InlineKeyboardButton(text='Четная неделя', callback_data='week_even'),
+        InlineKeyboardButton(text='В начало', callback_data='main'),
+    ],
+])
 select_day_keyboard = InlineKeyboardMarkup([
     [
         InlineKeyboardButton(text='Сегодня', callback_data='day_today'),
         InlineKeyboardButton(text='Завтра', callback_data='day_tomorrow'),
+        InlineKeyboardButton(text='В начало', callback_data='main'),
     ],
 ])
 
 async def start(update: Update, context):
     await update.message.reply_text('Введите номер группы:')
     return ENTER_GROUP
-
+async def restart(update: Update, context):
+    await update.callback_query.edit_message_text('Выберите какое расписание вам нужно:', reply_markup=select_schedule_type_keyboard)
+    return SELECT_SCHEDULE_TYPE
 async def enter_group(update: Update, context):
-    context.user_data['group'] = int(update.message.text)
+    try:
+        context.user_data['group'] = int(update.message.text)
+    except:
+        await update.message.reply_text('Неверный ввод, попробуйте еще раз:')
+        return ENTER_GROUP
+
     await update.message.reply_text('Выберите какое расписание вам нужно:', reply_markup=select_schedule_type_keyboard)
     return SELECT_SCHEDULE_TYPE
 
@@ -51,8 +65,10 @@ async def select_schedule_type(update: Update, context):
         await query.edit_message_text('Выберите день:', reply_markup=select_day_keyboard)
         return SELECT_DAY
     elif query.data == 'schedule_week':
-        await query.edit_message_text('*Типо расписание на неделю*')
-    return ConversationHandler.END
+        await query.edit_message_text('Выберите неделю:', reply_markup=select_week_keyboard)
+        return SELECT_WEEK
+    else:
+        return await restart(update, context)
 
 async def select_day(update: Update, context):
     query = update.callback_query
@@ -62,15 +78,27 @@ async def select_day(update: Update, context):
         await query.edit_message_text('*Типо расписание на сегодня*')
     elif query.data == 'day_tomorrow':
         await query.edit_message_text('*Типо расписание на завтра*')
+    else:
+        return await restart(update, context)
+async def select_week(update: Update, context):
+    query = update.callback_query
+    await query.answer()
 
-    return ConversationHandler.END
-
+    response = ''
+    if query.data == 'week_odd':
+        response = 'Расписание на нечетную неделю:\n\n' + sc.request_schedule_week(context.user_data['group'], 1)
+    elif query.data == 'week_even':
+        response = 'Расписание на четную неделю:\n\n' + sc.request_schedule_week(context.user_data['group'], 2)
+    else:
+        return await restart(update, context)
+    await query.edit_message_text(response, reply_markup=select_week_keyboard)
 
 conv_handler = ConversationHandler(
     entry_points=[CommandHandler('start', start)],
     states={
         ENTER_GROUP: [MessageHandler(filters.TEXT, enter_group)],
         SELECT_SCHEDULE_TYPE: [CallbackQueryHandler(select_schedule_type)],
+        SELECT_WEEK: [CallbackQueryHandler(select_week)],
         SELECT_DAY: [CallbackQueryHandler(select_day)]
     },
     fallbacks=[CommandHandler('start', start)]
